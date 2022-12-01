32450874aa6fe7bb49fc5a6379f5ab7c7bfbd479
EstebanSDeveloper <esteban.sarmientop@gmail.com>
2022-12-01 17:39:48 -0600

    Segundo Commit

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import CommonSection from "../components/UI/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+
+const Shop = () => {
+  return (
+    <Helmet title="Shop">
+      <CommonSection title="Products" />
+    </Helmet>
+  );
+};
+
+export default Shop;
